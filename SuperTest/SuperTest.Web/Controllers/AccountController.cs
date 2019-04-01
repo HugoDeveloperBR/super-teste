694d6cd4fa51f3545119c670dc4a5dd8a84dc0ea
Hugo <hugo.rodrigues.moura@gmail.com>
2019-04-01 12:28:50 -0300

    Criado controller Account

---
+++ SuperTest/SuperTest.Web/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperTest.Web.Controllers
+{
+    public class AccountController : BaseController
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}

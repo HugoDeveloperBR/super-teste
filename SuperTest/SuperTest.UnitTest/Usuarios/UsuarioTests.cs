70f6db6d6667c7b42dd952ef830514c7e910d249
Hugo <hugo.rodrigues.moura@gmail.com>
2019-03-31 21:10:32 -0300

    Alterado validacao de teste de Fact para Theory

--- SuperTest/SuperTest.UnitTest/Usuarios/UsuarioTests.cs
+++ SuperTest/SuperTest.UnitTest/Usuarios/UsuarioTests.cs
@@ -1,35 +1,25 @@
-﻿using SuperTest.ApplicationService;
-using SuperTest.Domain.Commands.Usuarios;
-using SuperTest.Domain.Services;
+﻿using SuperTest.Domain.Commands.Usuarios;
 using SuperTest.Domain.Validations.Usuarios;
-using System;
 using System.Linq;
-using System.Collections.Generic;
-using System.Text;
 using Xunit;
 
 namespace SuperTest.UnitTest.Usuarios
 {
     public class UsuarioTests
     {
-        [Fact(DisplayName ="")]
-        [Trait("","")]
-        public void Validation_CadastarNovoUsuario_UsuarioNaoPodeTerNomeVazio()
+        [Trait("Validation", "Cadastrar Novo Usuário")]
+        [Theory(DisplayName = "Usuário não pode ter nome vazio")]
+        [InlineData("", "email@provider.com", "95876974056", "abcd1234")]
+        [InlineData("    ", "email@provider.com", "95876974056", "abcd1234")]
+        public void Validation_CadastarNovoUsuario_UsuarioNaoPodeTerNomeVazio(string name, string email, string cpf, string senha)
         {
-            var command = new CadastrarNovoUsuarioCommand("", "email@provider.com", "40225701804", "125453");
+            var command = new CadastrarNovoUsuarioCommand(name, email, cpf, senha);
             var validation = new CadastrarNovoUsuarioValidation(command);
 
             validation.Validate();
 
             Assert.True(validation.Invalid);
-            Assert.Equal(1, validation.Notifications.Count);
             Assert.Equal("Nome não pode ser vazio", validation.Notifications.FirstOrDefault().Message);
-
-            command = new CadastrarNovoUsuarioCommand("Hugo Moura", "email@provider.com", "40225701804", "125453");
-            validation = new CadastrarNovoUsuarioValidation(command);
-            validation.Validate();
-
-            Assert.True(validation.Valid);
         }
     }
 }
